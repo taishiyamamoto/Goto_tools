e8deb78fffe1482c351f5aab450dcd31898d65f5
taishiyamamoto <ambitious0816@gmail.com>
2020-09-07 23:41:33 +0900

    add counter

--- randam_kidnap/src/kidnap.cpp
+++ randam_kidnap/src/kidnap.cpp
@@ -3,6 +3,7 @@
 #include <tf2_msgs/TFMessage.h>
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <jsk_rviz_plugins/OverlayMenu.h>
 
 #include <string>
 #include <random>
@@ -12,6 +13,7 @@ class RandamKidnap {
 		RandamKidnap(){
 			tf_sub = nh.subscribe("/tf",1,&RandamKidnap::TfCallback,this);
 			pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose",1);
+			menu_pub_ = nh.advertise<jsk_rviz_plugins::OverlayMenu>("kidnap_menu", 1);
 			gnss_sub_ = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>("gps/position",1,&RandamKidnap::GnssCallback,this);
 			gnss_pub_ = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("gps/position_kidnap",1);
 			nh.param("robto_frame", robot_frame, std::string("/base_link"));
@@ -20,6 +22,12 @@ class RandamKidnap {
 
 			create_noise_flag_ = false;
 			kidnap_flag_ = false;
+
+    		menu.action = jsk_rviz_plugins::OverlayMenu::ACTION_SELECT;
+			menu.current_index = 0;
+    		menu.menus.resize(1);
+    		menu.title = "Pseudo Kidnap Counter";
+			kidnap_counter_ = 0;
 		}
 		void ClickedCallback(const geometry_msgs::PointStampedConstPtr &point);
 		void TfCallback(const tf2_msgs::TFMessage &tf);
@@ -31,6 +39,8 @@ class RandamKidnap {
 		ros::Publisher pose_pub;
 		ros::Subscriber gnss_sub_;
 		ros::Publisher gnss_pub_;
+		ros::Publisher menu_pub_;
+		jsk_rviz_plugins::OverlayMenu menu;
 		geometry_msgs::PoseWithCovarianceStamped pose_t;
 
 		tf::TransformListener tf_listener;
@@ -38,6 +48,8 @@ class RandamKidnap {
 		std::string robot_frame;
 		static ros::Time now_time;
 		bool create_noise_flag_, kidnap_flag_;
+
+		int kidnap_counter_;
 
 		std::random_device seed_gen;
 		double CreateRanramNum(double min, double max);
@@ -57,8 +69,8 @@ RandamKidnap::GnssCallback(const geometry_msgs::PoseWithCovarianceStampedConstPt
 		kidnap_dis_x = CreateRanramNum(-15.0, 15.0);
 		kidnap_dis_y = CreateRanramNum(-15.0, 15.0);
 		dis = sqrt(kidnap_dis_x * kidnap_dis_x + kidnap_dis_y + kidnap_dis_y);
-		ROS_INFO("%lf,%lf",kidnap_dis_x, kidnap_dis_y);
-		ROS_INFO("%lf",dis);
+		//ROS_INFO("%lf,%lf",kidnap_dis_x, kidnap_dis_y);
+		//ROS_INFO("%lf",dis);
 	}while(dis <= 10.0);
 
 	gnss.pose.pose.position.x += kidnap_dis_x;
@@ -105,7 +117,7 @@ void RandamKidnap::TfCallback(const tf2_msgs::TFMessage &tf){
 		pose_t.pose.covariance[0] = pose_t.pose.covariance[7] = 0.25;
 		pose_t.pose.covariance[35] = 0.7;
 		//now_time = ros::Time::now();
-		if((ros::Time::now() - saved_time).toSec() > 60){
+		if((ros::Time::now() - saved_time).toSec() > 30){
 			//ROS_INFO("nya");
 			saved_time = ros::Time::now();
 
@@ -115,7 +127,7 @@ void RandamKidnap::TfCallback(const tf2_msgs::TFMessage &tf){
 			//int kidnap_dis_x,kidnap_dis_y;
 			//int dis;
 
-			
+			/*
 			do{
 				//ループは範囲の都合上行っている
 				kidnap_dis_x = CreateRanramNum(-10.0, 10.0);
@@ -124,9 +136,9 @@ void RandamKidnap::TfCallback(const tf2_msgs::TFMessage &tf){
 				ROS_INFO("%lf,%lf",kidnap_dis_x, kidnap_dis_y);
 				ROS_INFO("%lf",dis);
 			}while(dis <= 3.0);
-			
-			//kidnap_dis_x = CreateRanramNum(-50.0, 50.0);
-			//kidnap_dis_y = CreateRanramNum(-50.0, 50.0);
+			*/
+			kidnap_dis_x = CreateRanramNum(-50.0, 50.0);
+			kidnap_dis_y = CreateRanramNum(-50.0, 50.0);
 			dis = sqrt(kidnap_dis_x * kidnap_dis_x + kidnap_dis_y * kidnap_dis_y);
 			ROS_INFO("%lf,%lf",kidnap_dis_x, kidnap_dis_y);
 			ROS_INFO("%lf",dis);
@@ -141,7 +153,14 @@ void RandamKidnap::TfCallback(const tf2_msgs::TFMessage &tf){
 			quaternionTFToMsg(tf_quat,geo_quat);
 			pose_t.pose.pose.orientation = geo_quat;
 
-			//pose_pub.publish(pose_t);
+			kidnap_counter_++;
+			std::stringstream kidnap_ss; 
+      		kidnap_ss << kidnap_counter_;
+
+      		menu.menus[0] = kidnap_ss.str();
+			menu_pub_.publish(menu);
+
+			pose_pub.publish(pose_t);
 		}
 
 	}catch(tf::TransformException &e){
